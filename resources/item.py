767c87cee3c676d1b5d1984bbccd58ee8dfb505f
clioo <jesus_acosta1996@hotmail.com>
2020-06-10 12:27:35 -0700

    done for black

--- resources/item.py
+++ resources/item.py
@@ -1,37 +1,33 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import (
-    jwt_required,
-    get_jwt_claims,
-    get_jwt_identity,
-    jwt_optional,
-    fresh_jwt_required,
-)
+from flask_jwt_extended import jwt_required, fresh_jwt_required
 from models.item import ItemModel
+
+BLANK_ERROR = "'{}' cannot be blank."
+NAME_ALREADY_EXISTS = "An item with name '{}' already exists."
+ERROR_INSERTING = "An error occurred while inserting the item."
+ITEM_NOT_FOUND = "Item not found."
+ITEM_DELETED = "Item deleted."
 
 
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
-        "price", type=float, required=True, help="This field cannot be left blank!"
+        "price", type=float, required=True, help=BLANK_ERROR.format("price")
     )
     parser.add_argument(
-        "store_id", type=int, required=True, help="Every item needs a store_id."
+        "store_id", type=int, required=True, help=BLANK_ERROR.format("store_id")
     )
 
-    @jwt_required  # No longer needs brackets
-    def get(self, name):
+    def get(self, name: str):
         item = ItemModel.find_by_name(name)
         if item:
             return item.json(), 200
-        return {"message": "Item not found."}, 404
+        return {"message": ITEM_NOT_FOUND}, 404
 
     @fresh_jwt_required
-    def post(self, name):
+    def post(self, name: str):
         if ItemModel.find_by_name(name):
-            return (
-                {"message": "An item with name '{}' already exists.".format(name)},
-                400,
-            )
+            return {"message": NAME_ALREADY_EXISTS.format(name)}, 400
 
         data = Item.parser.parse_args()
 
@@ -40,23 +36,19 @@ class Item(Resource):
         try:
             item.save_to_db()
         except:
-            return {"message": "An error occurred while inserting the item."}, 500
+            return {"message": ERROR_INSERTING}, 500
 
         return item.json(), 201
 
     @jwt_required
-    def delete(self, name):
-        claims = get_jwt_claims()
-        if not claims["is_admin"]:
-            return {"message": "Admin privilege required."}, 401
-
+    def delete(self, name: str):
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
-            return {"message": "Item deleted."}, 200
-        return {"message": "Item not found."}, 404
+            return {"message": ITEM_DELETED}, 200
+        return {"message": ITEM_NOT_FOUND}, 404
 
-    def put(self, name):
+    def put(self, name: str):
         data = Item.parser.parse_args()
 
         item = ItemModel.find_by_name(name)
@@ -72,25 +64,5 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-    @jwt_optional
     def get(self):
-        """
-        Here we get the JWT identity, and then if the user is logged in (we were able to get an identity)
-        we return the entire item list.
-
-        Otherwise we just return the item names.
-
-        This could be done with e.g. see orders that have been placed, but not see details about the orders
-        unless the user has logged in.
-        """
-        user_id = get_jwt_identity()
-        items = [item.json() for item in ItemModel.find_all()]
-        if user_id:
-            return {"items": items}, 200
-        return (
-            {
-                "items": [item["name"] for item in items],
-                "message": "More data available if you log in.",
-            },
-            200,
-        )
+        return {"items": [item.json() for item in ItemModel.find_all()]}, 200
